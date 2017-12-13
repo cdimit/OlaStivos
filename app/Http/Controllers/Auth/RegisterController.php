e9474455f1b0264b18ac791120fba8d5869f3bf7
stavrinoust <stavrinoust@gmail.com>
2017-12-13 23:28:51 +0200

    Authentication and User Roles

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'password' => bcrypt($data['password']),
         ]);
 
-        $user->roles()->attach(Role::where('name', 'user')->first());
+        $user->roles()->attach(Role::where('name', 'admin')->first());
         return $user;
     }
 }
