913dd44e630fe8ab9352754f3d66d6f725c4e8a9
cdimit <cdimit07@cs.ucy.ac.cy>
2018-01-01 19:38:01 +0200

    calendar date format

--- resources/views/home/left-side-col.blade.php
+++ resources/views/home/left-side-col.blade.php
@@ -19,7 +19,7 @@
               @if($competition->date_start == $competition->date_finish)
                 <td>{{date('d.M', strtotime($competition->date_start))}}</td>
               @else
-                <td>{{date('d.M', strtotime($competition->date_start))}} - {{date('d.m', strtotime($competition->date_finish))}}</td>
+                <td>{{date('d.M', strtotime($competition->date_start))}} - {{date('d.M', strtotime($competition->date_finish))}}</td>
               @endif
               <td class="grow"><a href="/competition/{{$competition->id}}">{{$competition->name}}</a></td>
             </tr>
