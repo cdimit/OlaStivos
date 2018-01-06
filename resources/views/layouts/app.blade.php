68335663ff602b67328b93dca3cbd12cddc84079
cdimit <cdimit07@cs.ucy.ac.cy>
2018-01-07 00:41:24 +0200

    google ads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,13 @@
     <title>Ola Stivos</title>
 
     <!-- GOOGLE ADS -->
-
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-8960367603038090",
+        enable_page_level_ads: true
+      });
+    </script>
 
     <!-- Styles -->
 
