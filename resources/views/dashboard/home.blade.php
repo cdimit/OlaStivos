4a46ad03925e3f2512b522f27cebb7c81a3dfea5
cdimit <cdimit07@cs.ucy.ac.cy>
2018-03-12 23:08:49 +0200

    Dashboard Home - Some Infos added

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -2,5 +2,20 @@
 
 @section('content')
 	<h1>Dashboard Home</h1>
-@endsection
 
+	<div class="panel panel-default">
+  	<div class="panel-body">Athletes: {{App\Athlete::all()->count()}}</div>
+	</div>
+
+	<div class="panel panel-default">
+		<div class="panel-body">Results: {{App\Result::all()->count()}}</div>
+	</div>
+
+	<div class="panel panel-default">
+		<div class="panel-body">Competitions: {{App\Competition::all()->count()}}</div>
+	</div>
+
+	<div class="panel panel-default">
+		<div class="panel-body">Series: {{App\CompetitionSeries::all()->count()}}</div>
+	</div>
+@endsection
